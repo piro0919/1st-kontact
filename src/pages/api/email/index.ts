12a7f2d06e92aa53994cab4b15ad78ff9f6ddd12
piro <piro.haniwa@gmail.com>
2021-10-26 14:40:07 +0900

    feat: set timeout

--- src/pages/api/email/index.ts
+++ src/pages/api/email/index.ts
@@ -8,9 +8,12 @@ async function email(req: NextApiRequest, res: NextApiResponse): Promise<void> {
         pass: process.env.NODEMAILER_AUTH_PASS,
         user: process.env.NODEMAILER_AUTH_USER,
       },
+      connectionTimeout: 60 * 30,
+      greetingTimeout: 60 * 30,
       host: "smtp.mail.yahoo.co.jp",
       port: 465,
       secure: true,
+      socketTimeout: 60 * 30,
     });
     await transporter.sendMail({
       from: process.env.NODEMAILER_AUTH_USER,
