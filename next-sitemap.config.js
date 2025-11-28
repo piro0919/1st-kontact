805a1a3c89554faaafcc5becedb3eeb850b67fbb
piro0919 <piro.haniwa@gmail.com>
2025-11-28 22:09:31 +0900

    fix: update site url in sitemap configuration
    - Change the site URL from "https://www.natsuzolab.com" to "https://konta-niki.com" in next-sitemap.config.js to reflect the new domain.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.natsuzolab.com",
+  siteUrl: "https://konta-niki.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
